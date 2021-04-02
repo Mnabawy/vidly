05c2549e1eaba3fc95a576172caa4e903d9beab0
Mnabawy <m_ninja61@yahoo.com>
2021-04-02 11:00:20 +0200

    extracting form component

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import Input from "./input";
 import Joi from "joi-browser";
+import Form from "./common/form";
 
-class LoginForm extends Component {
+class LoginForm extends Form {
   state = {
-    account: {
+    data: {
       email: "",
       password: "",
     },
@@ -16,51 +17,13 @@ class LoginForm extends Component {
     password: Joi.string().required().label("Password"),
   };
 
-  validate = () => {
-    const options = {
-      abortEarly: false,
-    };
-    const { error } = Joi.validate(this.state.account, this.schema, options);
-    if (!error) return null;
-
-    let errors = {};
-    error.details.map((item) => (errors[item.path[0]] = item.message));
-    return errors;
-  };
-
-  validateProperty = ({ name, value }) => {
-    const obj = { [name]: value };
-    const schema = { [name]: this.schema[name] };
-    const { error } = Joi.validate(obj, schema);
-    return error ? error.details[0].message : null;
-    // console.log(error);
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-
-    let errors = this.validate();
-    this.setState({ errors: errors || {} });
-    if (errors) return;
-
+  doSubmit = () => {
     // call the server
     console.log("submitted");
   };
 
-  handleChange = ({ currentTarget: input }) => {
-    let errors = { ...this.state.errors };
-    let errorMessage = this.validateProperty(input);
-
-    if (errorMessage) errors[input.name] = errorMessage;
-    else delete errors[input.name];
-
-    let account = { ...this.state.account };
-    account[input.name] = input.value;
-    this.setState({ account, errors });
-  };
-
   render() {
-    const { account, errors } = this.state;
+    const { data, errors } = this.state;
     return (
       <>
         <h1>Login Form</h1>
@@ -69,7 +32,7 @@ class LoginForm extends Component {
             name="email"
             handleChange={this.handleChange}
             label="Email"
-            value={account.email}
+            value={data.email}
             errors={errors.email}
             type="email"
           />
@@ -77,11 +40,15 @@ class LoginForm extends Component {
             name="password"
             handleChange={this.handleChange}
             label="Password"
-            value={account.password}
+            value={data.password}
             errors={errors.password}
             type="password"
           />
-          <button type="submit" className="btn btn-primary">
+          <button
+            disabled={this.validate()}
+            type="submit"
+            className="btn btn-primary"
+          >
             Submit
           </button>
         </form>
